a6485aa61624857d169c24900afad93cf881d283
Yuliia <yuliia.a.horaichuk@gmail.com>
2025-04-11 20:51:58 +0100

    Rework welcome_display using MCV principles

--- GUI/login_display.py
+++ GUI/login_display.py
@@ -4,9 +4,14 @@ from user_auth import User
 class LoginDisplay(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
+        self.controller = None # set controller
 
-        self.configure(width=600, height=400)
+        #self.configure(width=600, height=400)
         self.grid_columnconfigure(0, weight=1)
 
         self.login_message = ctk.CTkLabel(self, text="\n Please enter your username and password:")
+        self.login_message.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.username_label = ctk.CTkLabel(self, text="Enter your username:")
+
+    def set_controller(self, controller):
+        self.controller = controller
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+
+from GUI.login_display import LoginDisplay
 from GUI.welcome_display import WelcomeDisplay
 
 # GUI App Window
@@ -6,20 +8,36 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+# Configure app window
         self.title("Vocabulary Builder")
         self.geometry("600x400")
-
-# Disable App window resizing
-        self.resizable(False, False)
+        self.resizable(False, False) #disable window resizing
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
+        self.current_window = None
+        self.load_welcome_display() # load welcome display when the app launches
+
+# Display welcome window
+    def load_welcome_display(self):
+        if self.current_window:
+            self.current_window.destroy() # destroy current window to avoid multiple windows being open simultaneously
+
+        self.current_window = WelcomeDisplay(self) # switch to welcome_display window
+        self.current_window.set_controller(self) # s
+        self.current_window.grid(row=0, column=0, sticky="nsew")
+
 # Display login window
-        self.welcome_display = WelcomeDisplay(self)
-        self.welcome_display.grid(row=0, column=0, sticky="nsew")
+    def load_login_display(self):
+        if self.current_window:
+            self.current_window.destroy() # destroy current window to avoid multiple windows being open simultaneously
 
+        self.current_window = LoginDisplay(self)
+        self.current_window.set_controller(self) # set App as controller
+        self.current_window.grid(row=0, column=0, sticky="nsew")
 
+# Run tkinter loop
 if __name__ == "__main__":
     app = App()
     app.mainloop()
